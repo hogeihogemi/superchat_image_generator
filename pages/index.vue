02634cd889f2261d3f0e6a8cb9095556afbc79a1
hogemi <hogeihogemi@gmail.com>
2021-05-01 23:04:15 +0900

    colorListを外に出した

--- pages/index.vue
+++ pages/index.vue
@@ -55,51 +55,20 @@ import { computed, defineComponent, reactive } from '@nuxtjs/composition-api'
 import SuperChat from '@/components/SuperChat.vue'
 import domtoimage from 'dom-to-image'
 import { saveAs } from 'file-saver'
+import { colorList } from '@/utils/constants'
 
 export default defineComponent({
   components: {
     SuperChat,
   },
   setup() {
-    const colorList = [
-      {
-        price: 200,
-        content: 'rgba(0,229,255,1)',
-        header: 'rgba(0,184,212,1)',
-      },
-      {
-        price: 500,
-        content: 'rgba(29,233,182,1)',
-        header: 'rgba(0,191,165,1)',
-      },
-      {
-        price: 1000,
-        content: 'rgba(255,202,40,1)',
-        header: 'rgba(255,179,0,1)',
-      },
-      {
-        price: 2000,
-        content: 'rgba(245,124,0,1)',
-        header: 'rgba(230,81,0,1)',
-      },
-      {
-        price: 5000,
-        content: 'rgba(233,30,99,1)',
-        header: 'rgba(194,24,91,1)',
-      },
-      {
-        price: 10000,
-        content: 'rgba(230,33,23,1)',
-        header: 'rgba(208,0,0,1)',
-      },
-    ]
-
     const state = reactive({
       content: 'スパチャです！',
       price: 200,
       username: '???????',
       iconUrl: '',
     })
+
     const savePng = () => {
       domtoimage.toBlob(document.getElementById('card')).then(function (blob) {
         saveAs(blob, `superChat_${new Date().getTime()}.png`)
---
+++ utils/constants.ts
@@ -0,0 +1,32 @@
+export const colorList = [
+  {
+    price: 200,
+    content: 'rgba(0,229,255,1)',
+    header: 'rgba(0,184,212,1)',
+  },
+  {
+    price: 500,
+    content: 'rgba(29,233,182,1)',
+    header: 'rgba(0,191,165,1)',
+  },
+  {
+    price: 1000,
+    content: 'rgba(255,202,40,1)',
+    header: 'rgba(255,179,0,1)',
+  },
+  {
+    price: 2000,
+    content: 'rgba(245,124,0,1)',
+    header: 'rgba(230,81,0,1)',
+  },
+  {
+    price: 5000,
+    content: 'rgba(233,30,99,1)',
+    header: 'rgba(194,24,91,1)',
+  },
+  {
+    price: 10000,
+    content: 'rgba(230,33,23,1)',
+    header: 'rgba(208,0,0,1)',
+  },
+]
