49fd1716a63df7d5b822567b2a7c7ee33d40e42a
hogemi <hogeihogemi@gmail.com>
2021-05-01 18:57:13 +0900

    金額に応じて色が変わるようにした

--- components/SuperChat.vue
+++ components/SuperChat.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="card" class="rounded">
-    <div class="header flex rounded-t py-2 px-4">
+    <div
+      class="header flex rounded-t py-2 px-4"
+      :style="{ 'background-color': props.headerColor }"
+    >
       <img
         src="@/assets/hogemi_superchat.jpg"
         class="icon mr-4 rounded-full h-10 w-10 flex items-center justify-center"
       />
       <div class="text-left">
         <div class="username">ほげいほげみ</div>
-        <div class="price">¥200</div>
+        <div class="price">¥{{ props.price }}</div>
       </div>
     </div>
-    <div class="content rounded-b text-left py-2 px-4">{{ props.content }}</div>
+    <div
+      class="content rounded-b text-left py-2 px-4"
+      :style="{ 'background-color': props.contentColor }"
+    >
+      {{ props.content }}
+    </div>
   </div>
 </template>
 
@@ -19,6 +27,9 @@ import { defineComponent } from '@nuxtjs/composition-api'
 export default defineComponent({
   props: {
     content: {},
+    contentColor: {},
+    headerColor: {},
+    price: {},
   },
   setup(props) {
     return {
@@ -30,12 +41,10 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .header {
-  background-color: #00b8d4;
   width: 579px;
   height: 56px;
 }
 .content {
-  background-color: #00e5ff;
   width: 579px;
   height: auto;
   font-size: 13px;
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="container">
-    <SuperChat :content="state.content" />
-
+    <SuperChat
+      :content="state.content"
+      :content-color="colorList[selectedPriceIndex].content"
+      :header-color="colorList[selectedPriceIndex].header"
+      :price="Number(state.price).toLocaleString()"
+    />
     <div>
       <input v-model="state.content" class="border" />
+      <select
+        :value="state.price"
+        @input="state.price = Number($event.target.value.replace(',', ''))"
+      >
+        <option v-for="color in colorList" :key="color.price">
+          {{ color.price }}
+        </option>
+      </select>
       <button @click="savePng">save</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from '@nuxtjs/composition-api'
+import { computed, defineComponent, reactive } from '@nuxtjs/composition-api'
 import SuperChat from '@/components/SuperChat.vue'
 import domtoimage from 'dom-to-image'
 import { saveAs } from 'file-saver'
@@ -20,17 +32,58 @@ export default defineComponent({
     SuperChat,
   },
   setup() {
+    const colorList = [
+      {
+        price: 200,
+        content: 'rgba(0,229,255,1)',
+        header: 'rgba(0,184,212,1)',
+      },
+      {
+        price: 500,
+        content: 'rgba(29,233,182,1)',
+        header: 'rgba(0,191,165,1)',
+      },
+      {
+        price: 1000,
+        content: 'rgba(255,202,40,1)',
+        header: 'rgba(255,179,0,1)',
+      },
+      {
+        price: 2000,
+        content: 'rgba(245,124,0,1)',
+        header: 'rgba(230,81,0,1)',
+      },
+      {
+        price: 5000,
+        content: 'rgba(233,30,99,1)',
+        header: 'rgba(194,24,91,1)',
+      },
+      {
+        price: 10000,
+        content: 'rgba(230,33,23,1)',
+        header: 'rgba(208,0,0,1)',
+      },
+    ]
+
     const state = reactive({
-      content: '',
+      content: 'スパチャです！',
+      price: 200,
     })
     const savePng = () => {
       domtoimage.toBlob(document.getElementById('card')).then(function (blob) {
         saveAs(blob, `superChat_${new Date().getTime()}.png`)
       })
     }
+
+    const selectedPriceIndex = computed(() => {
+      return colorList.findIndex((color) => color.price === state.price)
+    })
+
     return {
       savePng,
       state,
+      colorList,
+      selectedPriceIndex,
     }
   },
 })
